f1b767de45db493387a28b7587a75b925a2fce82
Thomas Heller <thomas.m.heller@gmail.com>
2020-05-15 18:30:37 +0200

    use own dmenu build for emoji_menu

--- emoji/emoji_menu.sh
+++ emoji/emoji_menu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-unicode=$(dmenu -i -l 30 <$HOME/.config/emoji/emoji.txt | sed 's/.*; //')
+unicode=$(/home/thomas/go/src/github.com/thomasheller/dmenu/dmenu -i -l 30 <$HOME/.config/emoji/emoji.txt | sed 's/.*; //')
 [ -z "$unicode" ] && exit
 for u in $unicode; do xdotool key U$u; done
