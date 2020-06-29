d098f15880180a2c63837540cfed7f56edd4d562
Thomas Heller <thomas.m.heller@gmail.com>
2020-06-29 17:46:59 +0200

    improve linkgrabber

--- linkgrabber.sh
+++ linkgrabber.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-urls=$(grep -aEo 'https?://[^[:space:]"'\'']+' | tac)
+urls=$(grep -aEo '(https?://[^[:space:]"'\'']+|([a-z0-9-]+\.)+[a-z]+)' | tac)
 [ -z "$urls" ] && exit
 url=$(echo "$urls" | dmenu -p 'Open:')
 [ -n "$url" ] && $BROWSER "$url"
